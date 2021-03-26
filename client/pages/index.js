70764dd74e7aa9637f85cabd96e73735760a0fc1
Konstantin Shvyryaev <konstantin.shvyryaev@yandex.ru>
2021-03-26 03:28:31 +0300

    Client added

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import buildClient from "../api/build-client";
+
+const Index = ({ currentUser }) => {
+    return currentUser
+        ? <h1>You are signed in</h1>
+        : <h1>You are not signed in</h1>;
+};
+
+Index.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser');
+    return data;
+};
+
+export default Index;
